166648ef2245bc81dd43a2b451f8bbde71bf7453
411pc20 <411pc20@10.1.4.47>
2016-09-16 19:09:49 -0500

    Modificacion del proyecto
    Se crea el proyecto que se va utilizar con el equipo de trabajo
    
    tipo:creacion

---
+++ CrudEquipo/src/com/eam/edu/Controlador/Controller.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eam.edu.Controlador;
+
+/**
+ *
+ * @author 411pc20
+ */
+public class Controller {
+    
+}
