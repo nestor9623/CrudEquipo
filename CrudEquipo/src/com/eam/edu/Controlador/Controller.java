35f75395b5524d3ea435dbc254b11715bb669733
411pc20 <411pc20@10.1.4.47>
2016-09-16 19:39:08 -0500

    Modificacion de la clase controlador
    Se crean los metodos del controlador para implementar el crud completo
    
    tipo:modificacion clase

--- CrudEquipo/src/com/eam/edu/Controlador/Controller.java
+++ CrudEquipo/src/com/eam/edu/Controlador/Controller.java
@@ -5,10 +5,60 @@
  */
 package com.eam.edu.Controlador;
 
+import com.eam.edu.dao.DaoEstudiante;
+import com.eam.edu.entidades.Estudiante;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 411pc20
  */
 public class Controller {
+   
+    private DaoEstudiante dao;
+    
+    
+    
+    
+    public void crear(Estudiante e){
+        int resultado= dao.create(e);
+        
+        if(resultado ==1){
+            System.out.println("registro el estudiante");
+        }
+  
+        
+    }
+    
+    public  Estudiante buscar(String cedula){
+        Estudiante es =dao.find(cedula);
+                return es;
+    }
+    
+    public boolean modificar(Estudiante e){
+        boolean modificado;
+       modificado= dao.edit(e);
+       if(modificado ==true){
+           System.out.println("SE MODIFICO");
+           return true;
+       }
+       
+       return false;
+    }
+    
+    
+    public boolean eliminar(String cedula){
+       
+        boolean eliminado;
+        eliminado = dao.remove(cedula);
+        
+        if(eliminado == true){
+            System.out.println("SE ELIMINO EL ESTUDIANTE");
+            return true;
+        }
+        
+        return false;
+    }
+    
     
 }
